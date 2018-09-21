00eee21703111eb40a6efcadbea9740c5367461e
artem <artemautonomia@gmail.com>
2018-09-22 01:23:18 +0300

    updated url rules uni

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -44,6 +44,11 @@ return [
                 '' => 'site/index',
                 'contact' => 'contact/index',
                 '<_a:login|logout>' => 'site/<_a>',
+
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
 
