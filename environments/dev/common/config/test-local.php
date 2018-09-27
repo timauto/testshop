c62e414cdd65bcd461e192db8ecee233eab906e0
artem <artemautonomia@gmail.com>
2018-09-27 15:33:24 +0300

    fix dbname for test

--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -6,7 +6,7 @@ return yii\helpers\ArrayHelper::merge(
     [
         'components' => [
             'db' => [
-                'dsn' => 'mysql:host=localhost;dbname=shop',
+                'dsn' => 'mysql:host=localhost;dbname=shop_test',
             ]
         ],
     ]
